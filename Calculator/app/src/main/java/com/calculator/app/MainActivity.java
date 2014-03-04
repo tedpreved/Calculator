31cd36d523cd13fb052951883f95223485013ee6
Teodor Mytsyk <teodor.mytsyk@gmai.com>
2014-03-04 23:40:13 +0200

    Turtorial

---
+++ Calculator/app/src/main/java/com/calculator/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.calculator.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+
+
+
+
+}
